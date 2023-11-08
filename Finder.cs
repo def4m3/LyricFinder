32efb5819b8a0167b8e9ccfea9a3a3b65b6853b8
Daniil yo <theduckhero789@gmail.com>
2023-11-08 16:37:53 +0300

    Добавьте файлы проекта.

---
+++ Finder.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Documents;
+using Windows.Media.Control;
+using Genius;
+using HtmlAgilityPack;
+using System.Text.RegularExpressions;
+using Genius.Models.Response;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+using System.Windows.Shapes;
+using System.Security.Policy;
+using Genius.Models;
+using System.Collections.Generic;
+
+namespace LyricFinder
+{
+    public class Finder
+    {
+        private readonly string APIKEY = "PUT_YOUR_API_KEY_HERE";
+        public Finder()
+        {
+        }
+        public async Task<List<SearchHit>> GetSongs()
+        {
+            var gsmtcsm = await GetSystemMediaTransportControlsSessionManager();
+            var mediaProperties = await GetMediaProperties(gsmtcsm.GetCurrentSession());
+
+            var songName = mediaProperties.Title;
+            var artistName = mediaProperties.Artist;
+
+            string pattern = @"(\[.*?\])|(\(.*?\))";
+            songName = Regex.Replace(songName, pattern, "").Trim();
+
+            var client = new GeniusClient(APIKEY);
+
+            SearchResponse searchResults = null;
+
+            if (songName.Contains("-") || songName.Contains("—"))
+            {
+                searchResults = await client.SearchClient.Search($"{songName}");
+            }
+            else
+            {
+                searchResults = await client.SearchClient.Search($"{artistName} {songName}");
+            }
+
+            var songs = searchResults.Response.Hits;
+
+            return songs;
+        }
+        public async Task<List<SearchHit>> GetSongs(string query)
+        {
+
+            var client = new GeniusClient(APIKEY);
+
+            SearchResponse searchResults = null;
+
+            searchResults = await client.SearchClient.Search(query);
+
+            var songs = searchResults.Response.Hits;
+
+            return songs;
+        }
+        public async Task<string> GetLyrics(ulong songId)
+        {
+            var client = new GeniusClient(APIKEY);
+
+            try
+            {
+                var song = await client.SongClient.GetSong(songId);
+
+                var web = new HtmlWeb();
+                var doc = web.Load(song.Response.Song.Url);
+                var lyricsJson = doc.DocumentNode.SelectSingleNode("//script[contains(text(), 'window.__PRELOADED_STATE__')]").InnerText;
+
+                var start = lyricsJson.IndexOf(@"<p>");
+                var end = lyricsJson.IndexOf(@",\""c") - 1;
+
+                var preloadedState = lyricsJson[start..end];
+                string lyricsHtml = Regex.Unescape(preloadedState);
+
+                MessageBox.Show("Найдена песня " + song.Response.Song.FullTitle, "Успех !", MessageBoxButton.OK, MessageBoxImage.None);
+
+                var lyrics = ConvertHtmlToPlainText(lyricsHtml);
+
+                return lyrics;
+
+
+            }
+            catch { return "Song is not found"; }
+        }
+        
+        private static string ConvertHtmlToPlainText(string html)
+        {
+            var document = new HtmlDocument();
+            document.LoadHtml(html);
+
+            return ConvertContentTo(document.DocumentNode);
+        }
+
+        private static string ConvertContentTo(HtmlNode node)
+        {
+            var innerText = new StringBuilder();
+
+            foreach (var subnode in node.ChildNodes)
+            {
+                switch (subnode.NodeType)
+                {
+                    case HtmlNodeType.Text:
+                        innerText.Append(subnode.InnerText);
+                        break;
+                    case HtmlNodeType.Element:
+                        innerText.Append(ConvertContentTo(subnode));
+                        break;
+                }
+            }
+
+            return innerText.ToString();
+        }
+        private static async Task<GlobalSystemMediaTransportControlsSessionManager> GetSystemMediaTransportControlsSessionManager()
+=> await GlobalSystemMediaTransportControlsSessionManager.RequestAsync();
+
+        private static async Task<GlobalSystemMediaTransportControlsSessionMediaProperties> GetMediaProperties(GlobalSystemMediaTransportControlsSession session)
+            => await session.TryGetMediaPropertiesAsync();
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Documents;
+using Windows.Media.Control;
+using Genius;
+using HtmlAgilityPack;
+using System.Text.RegularExpressions;
+using Genius.Models.Response;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+using System.Windows.Shapes;
+using Windows.Media.Protection.PlayReady;
+using Genius.Models.Song;
+using System.Xml.Linq;
+
+namespace LyricFinder
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string _lyricsSource;
+        private string _lyricsTranslated;
+        private Finder _finder;
+        private bool handleSelectionChanged = true;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _finder = new Finder();
+            translate_Button.IsEnabled = false;
+            back_Button.IsEnabled = false;
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            songs_ComboBox.Items.Clear();
+            _lyricsSource = string.Empty;
+            _lyricsTranslated = string.Empty;
+            translate_Button.IsEnabled = false;
+            back_Button.IsEnabled = false;
+
+            var songs = await _finder.GetSongs();
+
+            if (songs.Count == 0) return;
+
+            var lyrics = await _finder.GetLyrics(songs.FirstOrDefault().Result.Id);
+
+            if (lyrics == "Song is not found")
+            {
+                OutputBlock.Text = "Песня не найдена";
+                return;
+            }
+
+            string[] lines = lyrics.Split(@"\n");
+
+            OutputBlock.Inlines.Clear();
+            for (int i = 0; i < lines.Length; i++)
+            {
+                if (i > 0)
+                    OutputBlock.Inlines.Add(new LineBreak());
+                OutputBlock.Inlines.Add(new Run(lines[i]));
+            }
+
+            translate_Button.IsEnabled = true;
+
+            for (int i = 0; i < songs.Count; i++)
+            {
+                if (i == 0) SetSelectedItem(songs[i].Result);
+
+                var song = songs[i].Result;
+                songs_ComboBox.Items.Add(song);
+            }
+
+        }
+        private async void translate_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (_lyricsTranslated != string.Empty)
+            {
+                OutputBlock.Text = _lyricsTranslated;
+                translate_Button.IsEnabled = false;
+                back_Button.IsEnabled = true;
+                return;
+            }
+
+            _lyricsSource = OutputBlock.Text;
+
+            OutputBlock.Inlines.Clear();
+
+            using (var client = new HttpClient())
+            {
+                string fromLanguage = "auto";
+                string toLanguage = "ru";
+
+                string url = $"https://translate.googleapis.com/translate_a/single?client=gtx&sl={fromLanguage}&tl={toLanguage}&dt=t&q={Uri.EscapeUriString(_lyricsSource)}";
+
+                HttpResponseMessage response = await client.GetAsync(url);
+
+                response.EnsureSuccessStatusCode();
+
+                string responseBody = await response.Content.ReadAsStringAsync();
+
+                var translations = JArray.Parse(responseBody)[0];
+
+                foreach (var t in translations.Children())
+                {
+                    var translatedText = t.First?.ToString();
+                    if (!string.IsNullOrEmpty(translatedText))
+                    {
+                        OutputBlock.Inlines.Add(new Run(translatedText));
+                    }
+                }
+                _lyricsTranslated = OutputBlock.Text;
+                translate_Button.IsEnabled = false;
+                back_Button.IsEnabled = true;
+            }
+        }
+        private void Back_Button_Click(object sender, RoutedEventArgs e)
+        {
+            OutputBlock.Text = _lyricsSource;
+            translate_Button.IsEnabled = true;
+            back_Button.IsEnabled = false;
+        }
+        private async void manualInput_Button_Click(object sender, RoutedEventArgs e)
+        {
+            songs_ComboBox.Items.Clear();
+            Manualnput manualnput = new Manualnput();
+
+            if (manualnput.ShowDialog() == false) return;
+            if (manualnput.inputBox.Text == string.Empty) return;
+
+            _lyricsSource = string.Empty;
+            _lyricsTranslated = string.Empty;
+            translate_Button.IsEnabled = false;
+            back_Button.IsEnabled = false;
+
+            string query = manualnput.inputBox.Text;
+
+            var songs = _finder.GetSongs(query).Result;
+
+            var firstSong = songs.FirstOrDefault().Result;
+
+            var lyrics = await _finder.GetLyrics(firstSong.Id);
+
+            string[] lines = lyrics.Split(@"\n");
+
+            OutputBlock.Inlines.Clear();
+            for (int i = 0; i < lines.Length; i++)
+            {
+                if (i > 0)
+                    OutputBlock.Inlines.Add(new LineBreak());
+                OutputBlock.Inlines.Add(new Run(lines[i]));
+            }
+
+            translate_Button.IsEnabled = true;
+
+            for (int i = 0; i < songs.Count; i++)
+            {
+                if (i == 0) SetSelectedItem(songs[i].Result);
+
+                var song = songs[i].Result;
+                songs_ComboBox.Items.Add(song);
+            }
+
+        }
+
+        private async void songs_ComboBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            if (!handleSelectionChanged) return;
+            if (songs_ComboBox.SelectedItem == null) return;
+
+            var selectedSong = songs_ComboBox.SelectedItem as Song;
+
+            _lyricsSource = string.Empty;
+            _lyricsTranslated = string.Empty;
+            translate_Button.IsEnabled = false;
+            back_Button.IsEnabled = false;
+
+            var lyrics = await _finder.GetLyrics(selectedSong.Id);
+
+            string[] lines = lyrics.Split(@"\n");
+
+            OutputBlock.Inlines.Clear();
+            for (int i = 0; i < lines.Length; i++)
+            {
+                if (i > 0)
+                    OutputBlock.Inlines.Add(new LineBreak());
+                OutputBlock.Inlines.Add(new Run(lines[i]));
+            }
+
+            translate_Button.IsEnabled = true;
+        }
+
+        private async void SetSelectedItem(Song item)
+        {
+            handleSelectionChanged = false;
+            songs_ComboBox.SelectedItem = item;
+            await Task.Delay(100);
+            handleSelectionChanged = true;
+        }
+    }
+}
